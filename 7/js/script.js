26fbeaae6d0e3a27e122d5d733decd60e1bf5c01
Michele Cofano <brutalmachinelol@gmail.com>
2020-12-18 16:09:09 +0100

    Revisione 7° esercizio

---
+++ 7/js/script.js
@@ -0,0 +1,20 @@
+// stampa le potenze di 2 fino a 1000
+
+//var i = 0;
+
+
+for (var x = 1; x <= 1000; x++) { // ciclo for per il calcolo
+  console.log(Math.pow(2, x)); // calcolo della potenza di 2 fino a 1000
+}
+
+
+// --------- PRIMA PROVA PERSONALE -----------
+//for (var i = 0; Math.pow(2, i) <= 1000; i++) {
+//  console.log("2 alla", i, "=", Math.pow(2, i));
+//}
+
+// ---------- SECONDA PROVA PERSONALE -------------
+//while (Math.pow(2, i) <= 1000) {
+//    console.log("2 alla", i, "=", Math.pow(2, i));
+//    i++;
+//}
